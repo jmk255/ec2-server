3e79a1b5b3d45165caadd92bf163043287efd64b
jmk255 <jmk255@naver.com>
2024-10-23 18:06:13 +0900

    first

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 80;
+require('dotenv').config()
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT
+});
+
+//mysql 접속해서 데이터 조회
+app.get('/api', (req, res) => {
+  pool.query('select * from tbl_a', (err, results, fields) => {
+  console.log(err);
+  console.log(results);
+  console.log(fields);
+  res.json(results);//json으로 react로 보낸다
+  });
+});
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 대기중..`);
+});
